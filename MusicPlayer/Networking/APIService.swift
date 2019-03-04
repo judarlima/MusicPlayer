8409e75bf9dbebda4582381e82f01bf6f10a3d68
Judar Lima <judar.lima@ibm.com>
2019-03-04 10:17:16 -0300

    Creates Base Networking Layer.

---
+++ MusicPlayer/Networking/APIService.swift
@@ -0,0 +1,56 @@
+//
+//  APIService.swift
+//  MusicPlayer
+//
+//  Created by Judar Lima on 04/03/19.
+//  Copyright © 2019 raduJ. All rights reserved.
+//
+
+import Foundation
+
+protocol ServiceProtocol {
+    func requestData(with setup: ServiceSetup, completion: @escaping (Result<Data>) -> Void)
+}
+
+struct APIService: ServiceProtocol {
+    
+    public func requestData(with setup: ServiceSetup, completion: @escaping (Result<Data>) -> Void) {
+        guard let url = URL(string: setup.endpoint) else {
+            completion(.failure(.urlNotFound))
+            return
+        }
+        DispatchQueue.main.async {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    completion(.failure(.unknown(error.localizedDescription)))
+                }
+                
+                guard let data = data else {
+                    completion(.failure(.brokenData))
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    completion(.failure(.unknown("Could not cast to HTTPURLResponse object.")))
+                    return
+                }
+                
+                switch httpResponse.statusCode {
+                case 200...299:
+                    completion(.success(data))
+                    
+                case 403:
+                    completion(.failure(.authenticationRequired))
+                    
+                case 404:
+                    completion(.failure(.couldNotFindHost))
+                    
+                case 500:
+                    completion(.failure(.badRequest))
+                    
+                default: break
+                }
+                }.resume()
+        }
+    }
+}
---
+++ MusicPlayer/Networking/ServiceSetup.swift
@@ -0,0 +1,13 @@
+//
+//  ServiceSetup.swift
+//  MusicPlayer
+//
+//  Created by Judar Lima on 04/03/19.
+//  Copyright © 2019 raduJ. All rights reserved.
+//
+
+import Foundation
+
+protocol ServiceSetup {
+    var endpoint: String { get }
+}
