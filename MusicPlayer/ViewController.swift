e7944c44bf28eeed6cb22ecb20b0c423713a6274
Judar Lima <judar.lima@ibm.com>
2019-03-04 09:42:38 -0300

    Creates initial project.

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Judar Lima on 04/03/19.
+//  Copyright © 2019 raduJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
