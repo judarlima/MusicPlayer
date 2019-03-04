dee31cc4e1faa8b805b617383f3b16d1b406c8d5
Judar Lima <judar.lima@ibm.com>
2019-03-04 10:17:49 -0300

    Creates Coordinator.

--- MusicPlayer/Common/Coordinator/Coordinator.swift
+++ MusicPlayer/Common/Coordinator/Coordinator.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2019 raduJ. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol CoordinatorProtocol {
+    func start()
+    var navigationController: UINavigationController { get }
+}
+
+class Coordinator: CoordinatorProtocol {
+    let navigationController: UINavigationController
+    
+    func start() {
+        let playlistViewContoller = PlaylistViewController()
+        navigationController.pushViewController(playlistViewContoller, animated: false)
+    }
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+}
+
