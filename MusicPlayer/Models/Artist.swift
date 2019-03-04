d5024a73972b05b6e5db7ed154108e2030153794
Judar Lima <judar.lima@ibm.com>
2019-03-04 16:49:23 -0300

    Implements Artist Response Model

--- MusicPlayer/Models/Artist.swift
+++ MusicPlayer/Models/Artist.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Artist {
+    let id: Int
+    let name: String
+    let primaryGenre: String
+    let wrapperType: String
+}
+
+extension Artist: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case wrapperType
+        case primaryGenre = "primaryGenreName"
+        case name = "artistName"
+    }
+}
