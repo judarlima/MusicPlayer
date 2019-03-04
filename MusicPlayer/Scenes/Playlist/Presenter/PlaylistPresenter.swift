0754f046083515f58e97b07da87894328da53339
Judar Lima <judar.lima@ibm.com>
2019-03-04 19:05:38 -0300

    Implements Playlist Presenter

--- MusicPlayer/Scenes/Playlist/Presenter/PlaylistPresenter.swift
+++ MusicPlayer/Scenes/Playlist/Presenter/PlaylistPresenter.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+protocol PlaylistPresenterProtocol {
+    func presentPlaylist(playlist: Playlist)
+    func presentShuffled(playlist: Playlist)
+    func presentError(error: ServiceError)
+}
+
+class PlaylistPresenter: PlaylistPresenterProtocol {
+    weak var viewController: PlaylistDisplayProtocol?
+    
+    func presentError(error: ServiceError) {
+        viewController?.displayError(message: error.localizedDescription)
+    }
+    
+    func presentPlaylist(playlist: Playlist) {
+        let viewModel = PlayListViewModel(tracks: playlist.tracks.compactMap {
+            return TrackViewModel(songName: $0.name,
+                                  artist: $0.artistName + " (\($0.primaryGenre))", artwork: $0.artwork.absoluteString)
+        })
+        
+        viewController?.displayPlaylist(viewModel: viewModel)
+    }
+    
+    func presentShuffled(playlist: Playlist) { }
+}
