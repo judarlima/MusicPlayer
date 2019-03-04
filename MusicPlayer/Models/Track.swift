4e320881aaff00517e96aaaf697b3e4f3ffb2832
Judar Lima <judar.lima@ibm.com>
2019-03-04 16:49:57 -0300

    Implements Track response model.

--- MusicPlayer/Models/Track.swift
+++ MusicPlayer/Models/Track.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+struct Track {
+    let wrapperType: String
+    let id: Int
+    let artistName: String
+    let name: String
+    let artwork: URL
+    let primaryGenre: String
+}
+
+extension Track: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case wrapperType
+        case artistName
+        case name = "trackName"
+        case artwork = "artworkUrl"
+        case primaryGenre = "primaryGenreName"
+    }
+}
