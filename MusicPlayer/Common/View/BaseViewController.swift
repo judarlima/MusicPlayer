566bec4395e4e131075f1745aa9dbb350a6ef1fe
Judar Lima <judar.lima@ibm.com>
2019-03-04 09:58:57 -0300

    Creates BaseViewController and PlaylistViewController.

--- MusicPlayer/Common/View/BaseViewController.swift
+++ MusicPlayer/Common/View/BaseViewController.swift
@@ -9,22 +9,31 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-
+    
+    private var loaderView: UIView = UIView()
+    private let loader: UIActivityIndicatorView = UIActivityIndicatorView(style: .whiteLarge)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.loader.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        self.loaderView.backgroundColor = .purple
+        self.loaderView.addSubview(self.loader)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showLoader() {
+        self.loaderView.frame = UIScreen.main.bounds
+        self.loader.center = self.loaderView.center
+        
+        DispatchQueue.main.async {
+            self.loader.startAnimating()
+            self.view.addSubview(self.loaderView)
+        }
     }
-    */
-
+    
+    func hideLoader() {
+        DispatchQueue.main.async {
+            self.loader.stopAnimating()
+            self.loaderView.removeFromSuperview()
+        }
+    }
 }
--- MusicPlayer/Scenes/Playlist/PlaylistViewController.swift
+++ MusicPlayer/Scenes/Playlist/PlaylistViewController.swift
@@ -8,23 +8,19 @@
 
 import UIKit
 
-class PlaylistViewController: UIViewController {
+class PlaylistViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    init() {
+        super.init(nibName: "PlaylistViewController", bundle: Bundle.main)
+        self.view.backgroundColor = .lightGray
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
