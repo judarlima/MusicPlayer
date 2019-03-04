ae50d5ff9ba88eb22b6073db655edbb461336360
Judar Lima <judar.lima@ibm.com>
2019-03-04 16:50:17 -0300

    implements WrappedType response model.

--- MusicPlayer/Models/WrappedType.swift
+++ MusicPlayer/Models/WrappedType.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+enum WrappedType {
+    case artist(Artist)
+    case track(Track)    
+}
+
+extension WrappedType: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case wrapperType
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let type = try container.decode(String.self, forKey: .wrapperType)
+        if type == "artist" {
+            self = .artist(try Artist(from: decoder))
+        } else {
+            self = .track(try Track(from: decoder))
+        }
+    }
+}
