48da0ed153b56891a9f3ce92956c6d24fc9091f6
Judar Lima <judar.lima@ibm.com>
2019-03-04 19:01:40 -0300

    Implements UIImageView Extension to load downloaded images.

--- MusicPlayer/Common/Extensions/UIImage+Extensions.swift
+++ MusicPlayer/Common/Extensions/UIImage+Extensions.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2019 raduJ. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func loadImage(from url: String) {
+        guard let urlPath = URL(string: url) else { return }
+        self.image = UIImage(named: "placeholderImage")
+        ImageDownloader.shared.loadImage(from: urlPath) { [weak self] (image) in
+            guard let image = image else { return }
+            self?.alpha = 0
+            self?.image = image
+            UIView.animate(withDuration: 0.2, animations: {
+                self?.alpha = 1
+            })
+        }
+    }
+}
