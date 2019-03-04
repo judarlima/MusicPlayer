6e50483a9752a1cd30c69550aca6a35486573bc4
Judar Lima <judar.lima@ibm.com>
2019-03-04 19:09:25 -0300

    Instantiate the coordinator and set navigation as rootviewcontroller.

--- MusicPlayer/Supporting Files/AppDelegate.swift
+++ MusicPlayer/Supporting Files/AppDelegate.swift
@@ -14,11 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let viewController = PlaylistViewController()
+        let navigationController = UINavigationController()
+        let coordinator = Coordinator(navigationController: navigationController)
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = viewController
+        window?.rootViewController = coordinator.navigationController
         window?.makeKeyAndVisible()
+        
+        coordinator.start()
         
         return true
     }
