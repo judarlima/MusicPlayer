a95ef033db26212b8def3e5c0d5065647deb244f
Judar Lima <judar.lima@ibm.com>
2019-03-04 16:51:21 -0300

    Implements Playlist Service Setup.

--- MusicPlayer/Scenes/Playlist/Manager/PlaylistServiceSetup.swift
+++ MusicPlayer/Scenes/Playlist/Manager/PlaylistServiceSetup.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+enum PlaylistServiceSetup: ServiceSetup {
+    case tracksFromArtists(ids: [Int], limit: Int)
+    
+    var endpoint: String {
+        switch self {
+        case let .tracksFromArtists(ids, limit):
+            let url = API.URL.base + API.Path.playlist
+            return buildUrlString(url, ids: ids, limit: limit)
+        }
+    }
+    
+    private func buildUrlString(_ url: String, ids: [Int], limit: Int) -> String {
+        var urlComponents = URLComponents(string: url)
+        let idsString = ids.compactMap { String(describing: $0) }.joined(separator: ",")
+        let limitString = String(limit)
+        urlComponents?.queryItems = [URLQueryItem(name: "id", value: idsString),
+                                     URLQueryItem(name: "limit", value: limitString)]
+        guard let urlString = urlComponents?.url?.absoluteString else { return url }
+        return urlString
+    }
+}
