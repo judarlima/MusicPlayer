dedeff1191e4616ed0a2de74071b49a12d55a83e
Judar Lima <judar.lima@ibm.com>
2019-03-05 17:06:27 -0300

    Remove blank line

--- MusicPlayer/Scenes/Playlist/Views/TrackTableViewCell.swift
+++ MusicPlayer/Scenes/Playlist/Views/TrackTableViewCell.swift
@@ -26,5 +26,4 @@ class TrackTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-    
 }
