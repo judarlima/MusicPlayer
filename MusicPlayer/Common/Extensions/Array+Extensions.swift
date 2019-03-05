5c18e42ef2270c2d3bc5cdd84c1288709c5ba2b6
Judar Lima <judar.lima@ibm.com>
2019-03-05 17:04:23 -0300

    Creates an extension to handle with the shuffle algorithm

--- MusicPlayer/Common/Extensions/Array+Extensions.swift
+++ MusicPlayer/Common/Extensions/Array+Extensions.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+extension Array where Array.Element == Track {
+    
+    mutating func shuffleTracks() {
+        var dict = [String: [Track]]()
+        var result: [Track] = []
+        self.shuffle()
+        
+        for track in self {
+            if case nil = dict[track.artistName]?.append(track) {
+                dict[track.artistName] = [track]
+            }
+        }
+                
+        let limit = count / dict.keys.count
+        
+        for i in 0 ..< limit {
+            for (_, value) in dict {
+                result.append(value[i])
+            }
+        }
+        self = result
+    }
+}
